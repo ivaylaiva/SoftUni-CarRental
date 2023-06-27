08a2c57bdf9d9037da4249512368df86f988781f
ivyy <ivka20022002@abv.bg>
2023-06-27 13:36:09 +0300

    Creating ASP.NET Core Web project

---
+++ SoftUni-CarRental/SoftUni-CarRental/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftUni_CarRental.Models;
+using System.Diagnostics;
+
+namespace SoftUni_CarRental.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
