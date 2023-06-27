769b482150d9508064a384139bf5b65ca1047fc5
ivyy <ivka20022002@abv.bg>
2023-06-27 15:20:00 +0300

    Updating HomeController and Index.cshtml.

--- SoftUni-CarRental/SoftUni-CarRental/Controllers/HomeController.cs
+++ SoftUni-CarRental/SoftUni-CarRental/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace SoftUni_CarRental.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
