bedaf5ac3458800b93cc75d1a3d021bcb1ddf0ca
ivyy <ivka20022002@abv.bg>
2023-06-27 21:19:28 +0300

    Creating abstraction class and entity models.

---
+++ SoftUni-CarRental/SoftUni-CarRental.Models/Abstraction/EntityAbstraction.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftUni_CarRental.Models.Abstraction
+{
+    public abstract class EntityAbstraction
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ SoftUni-CarRental/SoftUni-CarRental.Models/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using SoftUni_CarRental.Models.Abstraction;
+
+namespace SoftUni_CarRental.Models.Models
+{
+    public class Car : EntityAbstraction
+    {
+
+        public string Model { get; set; } = null!;
+        public decimal PricePerDay { get; set; }
+        public int DoorsCount { get; set; }
+        public int PassengersCount { get; set; }
+        public string Colour { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+    }
+}
