85507a23cfd0aeeb9e052bc3b9a91142176d1bec
ivyy <ivka20022002@abv.bg>
2023-06-28 12:57:59 +0300

    Create DbContext.Add migration.Update Database.

---
+++ SoftUni-CarRental/SoftUni-CarRental.Database/DbContext/CarRentalDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SoftUni_CarRental.Models.Models;
+
+namespace SoftUni_CarRental.Data
+{
+    public class CarRentalDbContext : IdentityDbContext
+    {
+        public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarCard> CarCards { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
--- SoftUni-CarRental/SoftUni-CarRental/Program.cs
+++ SoftUni-CarRental/SoftUni-CarRental/Program.cs
@@ -12,12 +12,12 @@ namespace SoftUni_CarRental
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<CarRentalDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<CarRentalDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
