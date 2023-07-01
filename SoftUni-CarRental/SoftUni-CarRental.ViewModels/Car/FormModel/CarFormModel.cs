0c7c82f8729ed1978112202e87352232531a035f
ivyy <ivka20022002@abv.bg>
2023-07-01 10:36:43 +0300

    Updating CarFormModel viewModel.

--- SoftUni-CarRental/SoftUni-CarRental.ViewModels/Car/FormModel/CarFormModel.cs
+++ SoftUni-CarRental/SoftUni-CarRental.ViewModels/Car/FormModel/CarFormModel.cs
@@ -8,5 +8,6 @@ public class CarFormModel
     public int PassengersCount { get; set; }
     public string Colour { get; set; } = null!;
     public string Description { get; set; } = null!;
+    public string ImageUrl { get; set; } = null!;
     //public int PhotoId { get; set; }
 }
