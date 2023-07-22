79a343b8adbc6c0d0c86de2cdba93715fa595bd5
ivyy <ivka20022002@abv.bg>
2023-07-22 12:14:12 +0300

    Adding new project for tests.

---
+++ SoftUni-CarRental/SoftUni-CarRental.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SoftUni_CarRental.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
