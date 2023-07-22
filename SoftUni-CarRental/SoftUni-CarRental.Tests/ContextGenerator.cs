a959e772fc52934c1671d0e78b8d72da5ae50a99
ivyy <ivka20022002@abv.bg>
2023-07-22 12:33:23 +0300

    Adding new ContextGenerator class.

---
+++ SoftUni-CarRental/SoftUni-CarRental.Tests/ContextGenerator.cs
@@ -0,0 +1,14 @@
+﻿using SoftUni_CarRental.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni_CarRental.Tests
+{
+    public static class ContextGenerator
+    {
+          public static CarRentalDbContext
+    }
+}
