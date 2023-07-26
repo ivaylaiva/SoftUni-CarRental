e3a095fa5957806716cb9f3acc374618ca991b24
ivyy <ivka20022002@abv.bg>
2023-07-26 19:50:25 +0300

    Create tests for IdentityUserRolesService.

--- SoftUni-CarRental/SoftUni-CarRental.Tests/ServiceTests/IdentityRolesServiceTests.cs
+++ SoftUni-CarRental/SoftUni-CarRental.Tests/ServiceTests/IdentityRolesServiceTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SoftUni_CarRental.Models.Models;
 using SoftUni_CarRental.Services;
+using SoftUni_CarRental.Services.Interfaces;
 
 namespace SoftUni_CarRental.Tests.ServiceTests
 {
@@ -30,41 +31,6 @@ namespace SoftUni_CarRental.Tests.ServiceTests
 
 
             Assert.AreEqual(3, await db.Roles.CountAsync());
-        }
-        [Test]
-        public async Task CreateMemberUserTest()
-        {
-            var db = ContextGenerator.Instance;
-            var identityRolesService = new IdentityRolesService(db);
-            List<Role> roles = new List<Role>();
-            List<IdentityUserRole<string>> userRoles = new List<IdentityUserRole<string>>();
-
-            User user = new User()
-            {
-                UserName = "Test",
-                Email = "Test"
-            };
-
-
-            Role role = new Role()
-            {
-                Name = "Test",
-            };
-            roles.Add(role);
-
-
-            IdentityUserRole<string> identityUserRole = new IdentityUserRole<string>()
-            {
-                UserId = user.Id,
-                RoleId = roles.First(r => r.Name == "Test").Id
-            };
-            userRoles.Add(identityUserRole);
-            db.UserRoles.Add(identityUserRole);
-
-            await db.SaveChangesAsync();
-            identityRolesService.CreateMemberUser(user, roles, userRoles);
-
-            Assert.AreEqual(3, await db.UserRoles.CountAsync());
         }
     }
 }
---
+++ SoftUni-CarRental/SoftUni-CarRental.Tests/ServiceTests/IdentityUserRolesServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SoftUni_CarRental.Models.Models;
+using SoftUni_CarRental.Services;
+
+namespace SoftUni_CarRental.Tests.ServiceTests
+{
+    public class IdentityUserRolesServiceTests
+    {
+        [Test]
+        public async Task AllRolesTest()
+        {
+            var db = ContextGenerator.Instance;
+
+            var identityUserRolesService = new IdentityUserRoleService(db);
+
+            User user = new User()
+            {
+                UserName = "Test",
+                Email = "Test@abv.bg"
+            };
+            Role role = new Role()
+            {
+                Name = "Test",
+            };
+
+            IdentityUserRole<string> identityUserRole = new IdentityUserRole<string>()
+            {
+                UserId = user.Id,
+                RoleId = role.Id
+            };
+            db.UserRoles.Add(identityUserRole);
+            await db.SaveChangesAsync();
+
+            var result = identityUserRolesService.AllRoles();
+
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
